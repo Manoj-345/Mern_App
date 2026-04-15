0284bc4819d8608049e4ad712948304ad4c52a73
Manoj-345 <smanoj239150@gmail.com>
2026-04-15 11:38:48 +0530

    update

--- terraform/worker-userdata.sh
+++ terraform/worker-userdata.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-MASTER_IP="${master_ip}"
+
 
 # Install dependencies
 
@@ -59,14 +59,14 @@ systemctl enable kubelet
 
 echo "Waiting for master..."
 
-until nc -z ${MASTER_IP} 6443; do
+until nc -z ${master_ip} 6443; do
   sleep 10
 done
 
 
 # Join cluster (IMPORTANT 🔥)
 
-kubeadm join ${MASTER_IP}:6443 \
+kubeadm join ${master_ip}:6443 \
   --token y0mjcu.j833e3mwir4mkjic \
   --discovery-token-ca-cert-hash sha256:e5dbf6fe6885af4cb0f8d0a84db921b988722b2aced8dbd481c8af79a1f71175
 echo "Worker joined successfully!"
